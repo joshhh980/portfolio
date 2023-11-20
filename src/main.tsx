aa38f81e970e9ac9b273f41470544d94a6ffe326
Josh <joshhh980@gmail.com>
2023-11-20 17:16:16 +0000

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
+//@ts-ignore
 import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
